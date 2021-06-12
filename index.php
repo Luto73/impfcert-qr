5c2d83dfc56ab77e7f902c81c0be66b82d6a23d5
Luto <Luto73@users.noreply.github.com>
2021-06-12 16:44:31 +0200

    1st version which outputs qr code
    Although some variables are already defined, the data itself ist static

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
-include ('phpqrcode/qrlib.php')
+<?php 
+require "vendor/autoload.php";
+
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\Image\ImagickImageBackEnd;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
+
+$surname = "John";
+$name = "Doe";
+$birthday = "22.01.1948";
+$vaccine = "Comirnaty";
+$vaccdate = "06.04.2021";
+$dosenumber = 1;
+
+$renderer = new ImageRenderer(
+    new RendererStyle(400),
+    new ImagickImageBackEnd()
+);
+$writer = new Writer($renderer);
+// $writer->writeFile('Hello World!', 'qrcode.png');
+
+$writer->writeFile($surname."\t".$name."\t".$birthday."\t".$vaccine."\t".$vaccdate."\t".$dosenumber, "qr.png");
 ?>
